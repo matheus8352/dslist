1b93352e75d8f391a272b80abb8d62c1794cceb7
Matheus Alexandre Ribeiro Cunha <matheus@neoapp.com.br>
2023-05-10 00:06:47 -0300

    GET by id

--- src/main/java/dslist/dslist/controllers/GameController.java
+++ src/main/java/dslist/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dslist.dslist.dto.GameDTO;
 import dslist.dslist.dto.GameMinDTO;
 import dslist.dslist.services.GameService;
 
@@ -16,6 +18,11 @@ public class GameController {
 	
 	@Autowired
 	private GameService service;
+	
+	@GetMapping("/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		return service.findById(id);
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
--- src/main/java/dslist/dslist/services/GameService.java
+++ src/main/java/dslist/dslist/services/GameService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import dslist.dslist.dto.GameDTO;
 import dslist.dslist.dto.GameMinDTO;
+import dslist.dslist.entities.Game;
 import dslist.dslist.repositories.GameRepository;
 
 @Service
@@ -14,6 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository repository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game game = repository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Game not found"));
+		
+		return new GameDTO(game);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		return repository.findAll().stream().map(GameMinDTO::new).toList();
 	}
